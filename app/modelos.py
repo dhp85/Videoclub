3fb9ccb9f56f476bab48925707ab98ab0d115aac
Diego Herreros <diegohp85@gmail.com>
2024-07-18 23:46:20 +0200

    Feat: implemenatado clase Pelicula y test

---
+++ app/modelos.py
@@ -0,0 +1,105 @@
+from abc import ABC, abstractmethod
+import csv
+
+class Director:
+    def __init__(self, nombre: str, id: int = -1):
+        self.nombre = nombre
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f"Director ({self.id}): {self.nombre}"
+    
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            return self.id == other.id and self.nombre == other.nombre
+        return False
+    
+    def __hash__(self):
+        return hash((self.id, self.nombre))
+
+
+class Pelicula:
+    def __init__(self, titulo: str, sinopsis: str, director: object, id=-1):
+        self.titulo = titulo
+        self.sinopsis = sinopsis
+        self.id = id
+        self.director = director
+
+    def __repr__(self) -> str:
+            return f"Pelicula ({self.id}): {self.titulo}"
+    
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            return self.id == other.id and self.titulo == other.titulo
+        return False
+    
+    def __hash__(self):
+        return hash((self.id, self.titulo))    
+                
+    @property
+    def director(self):
+        return self._director
+    
+    @director.setter
+    def director(self, value):
+        if isinstance(value, Director):
+            self._director = value
+            self._id_director = value.id
+        elif isinstance(value, int):
+            self._director = None
+            self._id_director = value
+        else:
+            raise TypeError(f"{value} debe ser un entero o instancia de Director")
+
+
+class Dao(ABC):
+    """
+    @abstractmethod
+    def guardar(self, instancia):
+        pass
+    
+    @abstractmethod
+    def actualizar(self, intancia):
+        pass
+    
+    @abstractmethod
+    def borrar(self, id: int):
+        pass
+
+    @abstractmethod
+    def consultar(self, id: int):
+        pass
+    """
+    @abstractmethod
+    def todos(self):
+        pass
+
+
+class DaoCsvDirector(Dao):
+    def __init__(self, path):
+        self.path = path
+
+
+        
+    def todos(self):
+        with open(self.path, "r", newline="") as fichero:
+            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+            lista = []
+            for registro in lector_csv:
+                lista.append(Director(registro["nombre"], int(registro["id"])))
+        return lista
+    
+class DaoCsvPelicula(Dao):
+    def __init__(self, path):
+        self.path = path
+
+
+    
+    def todos(self):
+            with open(self.path, "r", newline="") as fichero:
+                lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+                lista = []
+                for registro in lector_csv:
+                    lista.append(Pelicula(registro["titulo"], registro["sinopsis"], int(registro["director_id"]), int(registro["id"])))
+            return lista
+    
---
+++ tests/test_modelos.py
@@ -0,0 +1,60 @@
+from app.modelos import Director, DaoCsvDirector, Pelicula, DaoCsvPelicula
+
+def test_create_director():
+    director = Director("Robert Redford")
+
+    assert director.nombre == "Robert Redford"
+    assert director.id == -1
+
+
+def test_dao_directores_traer_todos():
+    dao = DaoCsvDirector("tests/data/directores.csv")
+    directores = dao.todos()
+
+    assert len(directores) == 8
+    assert directores[7] == Director("Charlie Chaplin", 8) 
+
+def test_create_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", 9)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert pelicula.id == -1
+    assert pelicula.director is None
+  
+def test_create_pelicula_and_informar_director_completo():
+    director = Director("Peter Jackson", 9)
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", director)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert pelicula.id == -1
+    assert pelicula.director == director
+
+def test_asigna_director_a_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", -1)
+
+    director = Director("Peter Jackson", 9)
+
+    pelicula.director = director
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+
+    assert pelicula.id == -1
+    assert pelicula.director == director
+    assert pelicula._id_director == 9
+
+def test_dao_peliculas_traer_todos():
+    dao = DaoCsvPelicula("tests/data/peliculas-2.csv")
+    peliculas = dao.todos()
+
+    assert len(peliculas) == 5
+    assert peliculas[4] == Pelicula("Psicosis", "Sinopsis pelicula", 3, 24)
+
+
+
+
+ 
